c71ace3bf588a1406d4d36861d84e569aa8bf9bc
Edmundas Rakauskas <edmundas.rakauskas@gmail.com>
2019-06-21 12:44:53 +0300

    modifying Firebase dep version to be compatible with other project plugins

--- src/android/com/adobe/phonegap/push/PushInstanceIDListenerService.java
+++ src/android/com/adobe/phonegap/push/PushInstanceIDListenerService.java
@@ -1,24 +1,15 @@
 package com.adobe.phonegap.push;
-
-import android.content.Intent;
-import android.content.Context;
-import android.content.SharedPreferences;
 import android.util.Log;
+import com.google.firebase.messaging.FirebaseMessagingService;
 
-import com.google.firebase.iid.FirebaseInstanceId;
-import com.google.firebase.iid.FirebaseInstanceIdService;
-
-import org.json.JSONException;
-
-import java.io.IOException;
-
-public class PushInstanceIDListenerService extends FirebaseInstanceIdService implements PushConstants {
+public class PushInstanceIDListenerService extends FirebaseMessagingService implements PushConstants {
     public static final String LOG_TAG = "Push_InsIdService";
 
     @Override
-    public void onTokenRefresh() {
+    public void onNewToken(String token) {
+        super.onNewToken(token);
         // Get updated InstanceID token.
-        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        String refreshedToken = token;
         Log.d(LOG_TAG, "Refreshed token: " + refreshedToken);
         // TODO: Implement this method to send any registration to your app's servers.
         //sendRegistrationToServer(refreshedToken);
